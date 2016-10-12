3dea9c99bc29eb0345dbb1c2fdcd874e5bb1f3c7
RocPeng <1072313099@qq.com>
2016-10-12 09:12:42 +0800

    新增测试方法

--- Mytest/src/test/myTest.java
+++ Mytest/src/test/myTest.java
@@ -1,11 +1,14 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class myTest {
-
+	static ArrayList<Integer> list = new ArrayList<Integer>();
 	public static void main(String[] args) {
 //		List<String> L1=new ArrayList<>();
 //		List<String> L2=new ArrayList<>();
@@ -40,11 +43,71 @@ public class myTest {
 //		System.out.println(paramArr[0]);
 //		System.out.println(paramArr[1]);
 //		System.out.println(string.substring(paramArr[0].length()));
-		System.out.print("A Student (No:");
-		Scanner cin = new Scanner(System.in);
-		int a= cin.nextInt();
-		System.out.println(") Changes");
+//		System.out.print("A Student (No:");
+//		Scanner cin = new Scanner(System.in);
+//		int a= cin.nextInt();
+//		System.out.println(") Changes");
+//		iteratorTest();
+		 list.add(1);
+	        list.add(2);
+	        list.add(3);
+	        list.add(4);
+	        list.add(5);
+	        Thread thread1 = new Thread(){
+	            public void run() {
+	                Iterator<Integer> iterator = list.iterator();
+	                while(iterator.hasNext()){
+	                    Integer integer = iterator.next();
+	                    System.out.println(integer);
+	                    try {
+	                        Thread.sleep(100);
+	                    } catch (InterruptedException e) {
+	                        e.printStackTrace();
+	                    }
+	                }
+	            };
+	        };
+	        Thread thread2 = new Thread(){
+	            public void run() {
+	                Iterator<Integer> iterator = list.iterator();
+	                while(iterator.hasNext()){
+	                    Integer integer = iterator.next();
+	                    if(integer==2)
+	                        iterator.remove(); 
+	                }
+	            };
+	        };
+	        thread1.start();
+	        thread2.start();
+	}
+	
+	//ConcurrentModificationException“Ï≥£≤‚ ‘∑Ω∑®
+	public static void iteratorTest(){
+		List<String> list=new ArrayList<>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+//		for (String character:list) {
+//			if (character.equals("b")) {
+//				list.add("g");
+//			}
+//			System.out.println(character);
+//		}
 		
+		
+//		for (String character:list) {
+//			System.out.println(character);
+//		}
+		
+		Iterator<String> iterator=list.iterator();
+		while (iterator.hasNext()) {
+			String character=iterator.next();
+			System.out.println(character);
+			if (character.equals("b")) {
+				list.add("e");
+			}
+		}
 	}
 
 	static boolean itob(int i) {
